7d01e3f77b28fed19b579cc104b82cb5139a1bb2
sqrtofsaturn <sqrtofsaturn@gmail.com>
2015-05-26 12:02:26 -0700

    omg, it passes the 0 tests it has

---
+++ test/tentacle-protocol-buffer/tentacle-protocol-buffer_spec.cpp
@@ -0,0 +1,16 @@
+#include <limits.h>
+#include "gtest/gtest.h"
+#include "arduino-mock/Arduino.h"
+#include "arduino-mock/Serial.h"
+
+#define TOTAL_PINS 8
+// #include "../../src/pins.cpp"
+// #include "../../src/tentacle.cpp"
+
+
+using ::testing::Return;
+// using namespace tentacle;
+
+TEST(TentacleTest, Constructor) {
+  // Tentacle tentacle;
+}
