ab61c4f353e114bc978ca8e575dcfa734dabd6aa
sqrtofsaturn <sqrtofsaturn@gmail.com>
2015-05-29 12:44:15 -0700

    moar tests.

--- test/tentacle-protocol-buffer/tentacle-protocol-buffer_spec.cpp
+++ test/tentacle-protocol-buffer/tentacle-protocol-buffer_spec.cpp
@@ -13,11 +13,27 @@ using namespace tentacle;
 TEST(TentacleProtoBufTest, writeStateMessage_1) {
   uint8_t buffer[128];
   vector<tentacle::Pin> pins;
-  pins.push_back(Pin(4, 1, 5));
-  pins.push_back(Pin(40, 1, 15));
+  pins.push_back(Pin(4, 0, 1));
+  pins.push_back(Pin(40, 0, 0));
   TentacleProtoBuf tentacleProtoBuf(buffer, 128);
   unsigned int messageSize = tentacleProtoBuf.writeStateMessage(pins);
   vector<tentacle::Pin> pins2 = tentacleProtoBuf.readStateMessage(messageSize);
 
   EXPECT_EQ(pins2.size(), 2);
+}
+
+TEST(TentacleProtoBufTest, writeStateMessage_2) {
+  uint8_t buffer[128];
+  vector<tentacle::Pin> pins;
+  pins.push_back(Pin(5, 0, 0));
+  pins.push_back(Pin(8, 0, 1));
+  TentacleProtoBuf tentacleProtoBuf(buffer, 128);
+  unsigned int messageSize = tentacleProtoBuf.writeStateMessage(pins);
+  vector<tentacle::Pin> pins2 = tentacleProtoBuf.readStateMessage(messageSize);
+
+  EXPECT_EQ(pins2[0].getNumber(), pins[0].getNumber());
+  EXPECT_EQ(pins2[0].getValue(), pins[0].getValue());
+
+  EXPECT_EQ(pins2[1].getNumber(), pins[1].getNumber());
+  EXPECT_EQ(pins2[1].getValue(), pins[1].getValue());
 }
