f6348074224a9d5f6fd4a4961a41eb05c318a8ac
redaphid <a@octoblu.com>
2015-06-04 19:22:19 -0700

    WE DID IT

--- test/tentacle-protocol-buffer_spec.cpp
+++ test/tentacle-protocol-buffer_spec.cpp
@@ -24,9 +24,11 @@ TEST(TentacleProtoBufTest, writeStateMessage_1) {
   vector<tentacle::Pin> pins;
   pins.push_back(Pin(4, 0, 1));
   pins.push_back(Pin(40, 0, 0));
+
   TentacleProtoBuf tentacleProtoBuf(istream, ostream);
   cout << "writingMessage" << endl;
   tentacleProtoBuf.writeStateMessage(pins);
+
   cout << "sending to input stream" << endl;
   istream.write(obuffer, ostream.available());
   vector<tentacle::Pin> pins2 = tentacleProtoBuf.readStateMessage();
@@ -34,18 +36,31 @@ TEST(TentacleProtoBufTest, writeStateMessage_1) {
   EXPECT_EQ(pins2.size(), 2);
 }
 
-// TEST(TentacleProtoBufTest, writeStateMessage_2) {
-//   uint8_t buffer[128];
-//   vector<tentacle::Pin> pins;
-//   pins.push_back(Pin(5, 0, 0));
-//   pins.push_back(Pin(8, 0, 1));
-//   TentacleProtoBuf tentacleProtoBuf(buffer, 128);
-//   unsigned int messageSize = tentacleProtoBuf.writeStateMessage(pins);
-//   vector<tentacle::Pin> pins2 = tentacleProtoBuf.readStateMessage(messageSize);
-//
-//   EXPECT_EQ(pins2[0].getNumber(), pins[0].getNumber());
-//   EXPECT_EQ(pins2[0].getValue(), pins[0].getValue());
-//
-//   EXPECT_EQ(pins2[1].getNumber(), pins[1].getNumber());
-//   EXPECT_EQ(pins2[1].getValue(), pins[1].getValue());
-// }
+TEST(TentacleProtoBufTest, writeStateMessage_2) {
+  int length = 128;
+  uint8_t ibuffer[length];
+  uint8_t obuffer[length];
+
+  BufferStream istream(ibuffer, length);
+  BufferStream ostream(obuffer, length);
+
+  vector<tentacle::Pin> pins;
+
+  TentacleProtoBuf tentacleProtoBuf(istream, ostream);
+
+  pins.push_back(Pin(5, 0, 0));
+  pins.push_back(Pin(8, 0, 1));
+
+  unsigned int messageSize = tentacleProtoBuf.writeStateMessage(pins);
+
+  cout << "sending to input stream" << endl;
+  istream.write(obuffer, ostream.available());
+
+  vector<tentacle::Pin> pins2 = tentacleProtoBuf.readStateMessage();
+
+  EXPECT_EQ(pins2[0].getNumber(), pins[0].getNumber());
+  EXPECT_EQ(pins2[0].getValue(), pins[0].getValue());
+
+  EXPECT_EQ(pins2[1].getNumber(), pins[1].getNumber());
+  EXPECT_EQ(pins2[1].getValue(), pins[1].getValue());
+}
